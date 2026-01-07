d85ba8f91939cbefde2e39b7ea9fdf6840b93d9f
lumuslens <lumuslenstemp@gmail.com>
2026-01-07 11:46:37 +0530

    added refresh token endpoint

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path,include
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from custom_admin.views import LoginView,LogoutView,RegisterView,ProfileView,Getallusersview,GetallresellersView,ActivenowView
 urlpatterns = [
     path('register/',RegisterView.as_view()),
@@ -8,5 +11,6 @@ urlpatterns = [
     path('profile/',ProfileView.as_view()),
     path('users/',Getallusersview.as_view()),
     path('resellers/',GetallresellersView.as_view()),
-    path('active',ActivenowView.as_view())
+    path('active',ActivenowView.as_view()),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
